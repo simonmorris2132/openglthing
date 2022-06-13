80338314300138ec346e333d5a1972cb623d87a1
Simon Morris <simonjmorris213@gmail.com>
2022-06-12 21:39:57 -0500

    commit

--- src/com/base/engine/renderengine/Renderer.java
+++ src/com/base/engine/renderengine/Renderer.java
@@ -7,8 +7,8 @@ import org.lwjgl.opengl.GL30;
 public class Renderer {
     
     public void prepare() {
-        GL11.glClearColor(1, 0, 0, 1);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+        GL11.glClearColor(1, 0, 0, 1);
     }
 
     public void render(RawModel rawModel) {
