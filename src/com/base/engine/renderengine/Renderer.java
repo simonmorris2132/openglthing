da858c3b07c9b50fa6bd6fddb48611afc5f7bae4
Simon Morris <simonjmorris213@gmail.com>
2022-06-12 21:28:17 -0500

    commit

--- src/com/base/engine/renderengine/Renderer.java
+++ src/com/base/engine/renderengine/Renderer.java
@@ -8,6 +8,7 @@ public class Renderer {
     
     public void prepare() {
         GL11.glClearColor(1, 0, 0, 1);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
     }
 
     public void render(RawModel rawModel) {
